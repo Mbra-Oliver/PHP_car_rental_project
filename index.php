758fc324c5209dba943c2a862c7fc2bfe94aa9b3
Mbra-Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2022-07-05 21:56:16 +0000

    Project setup

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Car rental Project</title>
+    <link rel="stylesheet" href="./assets/css/login.css">
+</head>
+<body>
+
+<h1>Login To Your Dashboard</h1>
+<form>
+  <div class="row">
+    <label for="email">Email</label>
+    <input type="email" name="email" autocomplete="off" placeholder="email@example.com">
+  </div>
+  <div class="row">
+    <label for="password">Password</label>
+    <input type="password" name="password">
+  </div>
+  <div style="display: flex; color:#8086a9;
+  ">
+    <input type="checkbox" name="log_admin">
+    <label for="">Log as admin</label>
+  </div>
+  <button type="submit">Login</button>
+</form>
+    
+</body>
+</html>
