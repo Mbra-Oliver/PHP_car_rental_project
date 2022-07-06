d2097eb7ab2ce377ed9bbd12210ea4cf63545dcd
Mbra-Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2022-07-06 21:48:54 +0000

    Chapter I - From Login To Admin Dashboard

---
+++ configs/database.php
@@ -0,0 +1,5 @@
+<?php 
+
+
+$databaseConnexion= new PDO('mysql:host=127.0.0.1;dbname=car_rental', 'root', '');
+?>
---
+++ views/admin/includes/footer.php
@@ -0,0 +1,30 @@
+<footer class="footer text-center"> 2021 © Ample Admin brought to you by <a
+                    href="https://www.wrappixel.com/">wrappixel.com</a>
+            </footer>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../../assets/admins/plugins/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../../assets/admins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../assets/admins/js/app-style-switcher.js"></script>
+    <!--Wave Effects -->
+    <script src="../../assets/admins/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="../../assets/admins/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../../assets/admins/js/custom.js"></script>
+</body>
+
+</html>
